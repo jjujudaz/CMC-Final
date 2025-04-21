99b109f8d449348c9acc1eee991e4b9c8d6db56c
Deon <102772259@student.swin.edu.au>
2025-04-21 15:09:40 +1000

    Styling is done for the profile page. The fonts are currently working using useFont from expo-font which can be viewed in _layout.tsx. Fonts that are used are found in the assets/fonts folder.
    Improvements needed:
    - Have an edit button to edit the profile details.
    - Profile details shouldn't be hard-coded.
    - Currently the profile screen is only accessible through the router pushing the profile page.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,34 @@
 import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
 import "./global.css";
+import { useEffect } from "react";
 
 function RootLayout() {
+  // Load custom fonts.
+  const [loaded, error] = useFonts({
+    'OpenSans-Regular': require('../assets/fonts/OpenSans-Regular.ttf'),
+    'Roboto-Regular': require('../assets/fonts/Roboto-Regular.ttf'),
+    'Urbanist-Regular': require('../assets/fonts/Urbanist-Regular.ttf')
+  });
+
+  // Log any errors that occur during font loading.
+  useEffect(() => {
+    if (error) {
+      console.error("Error loading fonts:", error);
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
     <Stack>
       <Stack.Screen name="register" options={{ title: "Register" }} />
       <Stack.Screen name="index" options={{ headerShown: false }} />
       <Stack.Screen name="home" options={{ title: "Home" }} />
       <Stack.Screen name="login" options={{ title: "Login" }} />
+      <Stack.Screen name="profile" options={{ title: "Profile" }} />
     </Stack>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   // NOTE: Update this to include the paths to all of your component files.
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
@@ -6,17 +7,12 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-primary: '#16519f',
-secondary:'#f07e74',
-tertiary: '#f8dd2e',
-lightPrimary: '#4fcbe9',
-      },
-      fontFamily: {
-        Title: ['Urbanist'],
-        Menu: ['Roboto'],
-        Text: ['Open Sans'],
+        primary: '#16519f',
+        secondary:'#f07e74',
+        tertiary: '#f8dd2e',
+        lightPrimary: '#4fcbe9',
       },
     },
   },
-  plugins: [],
+  plugins: []
 };
