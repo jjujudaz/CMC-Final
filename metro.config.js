eee79304b1d6579bbe5bfbe8b83a4aa3c038210b
Yaser Haidari <104608220@student.swin.edu.au>
2025-05-12 20:35:02 +1000

    works fine basic project went 5 versions back

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,6 @@
-const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require("nativewind/metro");
+const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
+config.resolver.unstable_enablePackageExports = false;
 
-module.exports = withNativeWind(config, { input: "./app/global.css" });
+module.exports = config;
